60c3fbbeb44b8b48fb881ba1ed8fa7f42421f82b
Tim Paine <t.paine154@gmail.com>
2020-05-18 13:06:57 -0400

    fixes #18

---
+++ js/src/model.ts
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/explicit-member-accessibility */
+/* eslint-disable @typescript-eslint/tslint/config */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import {DOMWidgetModel, ISerializers} from "@jupyter-widgets/base";
+import {MODULE_VERSION} from "./version";
+
+
+export
+class DagreD3Model extends DOMWidgetModel {
+    static serializers: ISerializers = {
+        ...DOMWidgetModel.serializers,
+        // Add any extra serializers here
+    };
+
+    static modelName = "DagreD3Model";
+    static modelModule = "ipydagred3";
+    static modelModuleVersion = MODULE_VERSION;
+    static viewName = "DagreD3View";   // Set to null if no view
+    static viewModule = "ipydagred3";   // Set to null if no view
+    static viewModuleVersion = MODULE_VERSION;
+
+    public defaults() {
+        return {...super.defaults(),
+            _model_name: DagreD3Model.modelName,
+            _model_module: DagreD3Model.modelModule,
+            _model_module_version: DagreD3Model.modelModuleVersion,
+            _view_name: DagreD3Model.viewName,
+            _view_module: DagreD3Model.viewModule,
+            _view_module_version: DagreD3Model.viewModuleVersion,
+        };
+    }
+}
+
--- src/widget.ts
+++
@@ -1,7 +0,0 @@
-// tslint:disable: max-classes-per-file
-export {DagreD3Model} from './model';
-export {DagreD3View} from './view';
-
-
-// Import the CSS
-import '../css/widget.css'
