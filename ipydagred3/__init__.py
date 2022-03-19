2d6bbdfa8da67fbaeb45727d414becd0377342d1
Tim Paine <t.paine154@gmail.com>
2022-03-19 17:44:44 -0400

    readd nbextension init and _version script

--- ipydagred3/__init__.py
+++ ipydagred3/__init__.py
@@ -1,9 +1,7 @@
 # coding: utf-8
 
-__version__ = "0.3.1"
-
-
 from .widget import DagreD3Widget  # noqa: F401
+from ._version import __version__  # noqa: F401
 from .nbextension import _jupyter_nbextension_paths  # noqa: F401
 
 from .graph import Graph  # noqa: F401
